0d488e26e75fccb2716e97ef22246a2c013a1f8a
Najat <najatmuahhid@gmail.com>
2023-12-03 22:20:02 +0600

    Print only shop name instead of full URL

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
         <div>
-                <h4 class="text-center mt-5">Shop Name is <b>{{ $shopDomain ?? Auth::user()->name }}</b></h4>
+                <h4 class="text-center mt-5">Shop Name is <b>{{ preg_replace('/.myshopify.com$/', '', Auth::user()->name) }}</b></h4>
         </div>
         <div>
                 <h4 class="text-center">Shop ID is <b>{{ Auth::user()->id }}</h4>
